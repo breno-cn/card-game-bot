4c69586290e676de107afa3c07889c88c5380c5a
Breno <breno.cn@hotmail.com>
2021-06-28 18:41:21 -0300

    init

---
+++ lib/truco.ex
@@ -0,0 +1,18 @@
+defmodule Truco do
+  @moduledoc """
+  Documentation for `Truco`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Truco.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
